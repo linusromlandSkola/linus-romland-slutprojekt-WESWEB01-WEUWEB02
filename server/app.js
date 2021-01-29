9d497f561710f7529d2d0f1244adab5df25c40c3
Linus Romland <romlandlinus@gmail.com>
2021-01-29 09:29:35 +0100

    Backend for Login

--- server/app.js
+++ server/app.js
@@ -1,6 +1,127 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const dBModule = require("./dbModule.js");
+const User = require("./models/user.js");
+const fs = require("fs");
+const session = require("express-session");
+const flash = require("express-flash");
+const sessionstore = require("sessionstore");
+const passport = require("passport");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+//Initializes the Store Varible for use with sessionstore
+//Connectes to MongoDB
+let store;
+connectToMongo("FileUpload");
+
+//Sets and uses dependencies etc.
+app.use(flash());
+app.use(express.static("client"));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "keyboard cat",
+    store: store,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      secure: false,
+      //domain: ".romland.space",
+      sameSite: "strict",
+    },
+  })
+);
+app.use(passport.initialize(undefined));
+app.use(passport.session(undefined));
+
+const initializePassport = require("./config/passport.js");
+initializePassport(
+  passport,
+  (name) => User.find((user) => user.name === name),
+  (id) => User.find((user) => user.id === id)
+);
+
+app.post("/register", async (req, res) => {
+  try {
+    const userExist = await dBModule.findInDBOne(User, req.body.name);
+    console.log(req.body.password);
+    if (userExist == null) {
+      dBModule.saveToDB(createUser(req.body.name, req.body.password));
+      res.sendStatus(201);
+      console.log("Got here");
+    } else {
+      res.sendStatus(409);
+    }
+  } catch {
+    res.sendStatus(500);
+  }
+});
+
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.get("/auth", checkAuthenticated, async (req, res) => {
+  let tmp = await req.user;
+  res.send("You're authenticated as " + tmp.name);
+});
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.sendFile(__dirname + "/client/register.html");
+});
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.sendFile(__dirname + "/client/login.html");
+});
+
+app.get("/logout", checkAuthenticated, (req, res) => {
+  req.logOut();
+  res.send("You are now logged out");
+});
+
+function connectToMongo(dbName) {
+  if (fs.existsSync("mongoauth.json")) {
+    const mongAuth = require("./mongoauth.json");
+    dBModule.cnctDBAuth(dbName);
+    store = sessionstore.createSessionStore({
+      type: "mongodb",
+      authSource: "admin",
+      username: mongAuth.username,
+      password: mongAuth.pass,
+    });
+  } else {
+    dBModule.cnctDB(dbName);
+    store = sessionstore.createSessionStore({ type: "mongodb" });
+  }
+}
+
+function createUser(nameIN, passIN) {
+  return new User({
+    name: nameIN,
+    password: passIN,
+  });
+}
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/auth");
+  }
+  next();
+}
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
---
+++ server/dbModule.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const ObjectID = require("mongodb").ObjectID;
+let db;
+
+//Connect to MongoDB With Authentication.
+exports.cnctDBAuth = (collectionname) => {
+  const mongAuth = require("./mongoauth.json");
+  mongoose.connect("mongodb://localhost:27017/" + collectionname, {
+    auth: {
+      authSource: "admin",
+    },
+    user: mongAuth.username,
+    pass: mongAuth.pass,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function () {
+    console.log("Connected to MongoDB using collection " + collectionname);
+  });
+};
+
+//Connect to MongoDB
+exports.cnctDB = (collectionname) => {
+  let dbLink = `mongodb://localhost/${collectionname}`;
+  mongoose.connect(dbLink, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function () {
+    console.log("Connected to MongoDB using " + collectionname);
+  });
+};
+
+//Finds "toFind" in Database on the Model provided
+exports.findInDBOne = async (Model, toFind) => {
+  return await Model.findOne({ name: toFind });
+};
+
+exports.findInDB = async (Model) => {
+  return await Model.find({});
+};
+
+exports.findUserWithID = async (Model, toFind) => {
+  return await Model.findOne({ _id: toFind });
+};
+
+//takes input with type Model. Saves that model in Database. Cant be used before cnctDB or cnctDBAuth.
+exports.saveToDB = (input) => {
+  input.save(() => {});
+};
