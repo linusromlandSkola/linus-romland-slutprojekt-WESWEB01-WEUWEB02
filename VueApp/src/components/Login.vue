82e9ca83395270c4923df790c8463a277ed4e057
Linus Romland <romlandlinus@gmail.com>
2021-01-29 13:12:14 +0100

    Added Start of Login Page with vue

---
+++ VueApp/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <form id="theForm">
+      <input
+        type="text"
+        placeholder="Username"
+        class="fadeIn second"
+        id="username"
+        name="username"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        class="fadeIn third"
+        id="password"
+        name="password"
+      />
+      <p id="errorText"></p>
+      <input type="submit" class="fadeIn fourth" value="Log In" />
+    </form>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
