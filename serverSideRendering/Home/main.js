565c682d3b306d35dd099a5c13ba0d528c4e9027
Linus Romland <romlandlinus@gmail.com>
2021-01-30 13:45:02 +0100

    wokring without external .vue file

--- serverSideRendering/Home/main.js
+++ serverSideRendering/Home/main.js
@@ -2,6 +2,6 @@ const Vue = require('vue')
 const fs = require("fs")
 module.exports = function createApp () {
   return new Vue({
-    render: h => h(fs.readFileSync("./Home/App.vue")),
-  }).$mount('#app')
+    template: `<div>The visited URL is: aaa</div>`
+  })
 }
