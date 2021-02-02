17bd548770f0f8d15d57f76deb65740a44f63043
Linus Romland <romlandlinus@gmail.com>
2021-02-02 12:33:42 +0100

    testing of exports instead of module and changed some importnas

--- serverSideRendering/Home/main.js
+++ serverSideRendering/Home/main.js
@@ -1,11 +1,9 @@
 const Vue = require('vue')
-const fs = require("fs")
-//const App = require('./App.vue')
+//import Vue from 'vue'
+//import App from "./App.vue"
+const App = require('./App.vue')
 
-module.exports = async function createApp () {
-  //let tmp = await fs.readFileSync("./Home/App.vue")
-  import App from "./Home/App.vue"
-  //console.log(tmp)
+exports.createApp = () => {
   return new Vue({
     render: h => h(App),
   }).$mount('#app')
