338d2112bd24e47c6b25dc2d6a56f87452a6866b
Linus Romland <romlandlinus@gmail.com>
2021-02-13 20:28:01 +0100

    Revert "broke it even more"
    This reverts commit 160a969c3eac5ddfab8f18d24c0e4495390d9b4f.

--- serverSideRendering/Home/main.js
+++ serverSideRendering/Home/main.js
@@ -1,11 +1,8 @@
 const Vue = require('vue')
 const fs = require("fs")
-//const App = require('./App.vue')
+const App = require('./App.vue')
 
-module.exports = async function createApp () {
-  //let tmp = await fs.readFileSync("./Home/App.vue")
-  import App from "./Home/App.vue"
-  //console.log(tmp)
+module.exports = function createApp () {
   return new Vue({
     render: h => h(App),
   }).$mount('#app')
