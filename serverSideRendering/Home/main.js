b4d692409d3cff3aadcdb6a515a3f83c6a985a26
Linus Romland <romlandlinus@gmail.com>
2021-02-13 20:38:51 +0100

    i hate serverside rendienrgjshadhjashdkjashdasdawsdasdasdasadsadsadsadsdas

--- serverSideRendering/Home/main.js
+++ serverSideRendering/Home/main.js
@@ -1,7 +1,11 @@
-const Vue = require('vue')
-const fs = require("fs")
+import Vue from 'vue'
+import App from '../Home/App.vue'
+
 module.exports = function createApp () {
-  return new Vue({
+  /*return new Vue({
     template: `<div>The visited URL is: aaa</div>`
-  })
+  })*/
+  return new Vue({
+    render: h => h(App),
+  }).$mount('#app')
 }
