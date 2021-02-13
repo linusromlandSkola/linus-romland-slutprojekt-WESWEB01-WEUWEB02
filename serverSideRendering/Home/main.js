1587e40d83f2fa09d74c18e1bdf013c00be04313
Linus Romland <romlandlinus@gmail.com>
2021-02-13 20:28:05 +0100

    Revert "trying with .vue app again"
    This reverts commit a5c697682ef08c05c58570deeea79b77efdc9da3.

--- serverSideRendering/Home/main.js
+++ serverSideRendering/Home/main.js
@@ -1,9 +1,7 @@
 const Vue = require('vue')
 const fs = require("fs")
-const App = require('./App.vue')
-
 module.exports = function createApp () {
   return new Vue({
-    render: h => h(App),
-  }).$mount('#app')
+    template: `<div>The visited URL is: aaa</div>`
+  })
 }
