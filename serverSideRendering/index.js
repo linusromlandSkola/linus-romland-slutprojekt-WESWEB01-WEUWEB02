10a63707a674d6d0aa73c5ab701bf4b86b899e4a
Linus Romland <romlandlinus@gmail.com>
2021-02-13 20:27:55 +0100

    Revert "testing vueexpress"
    This reverts commit 4c6eade8e50b0e51dd7f416a096a154bff651675.

--- serverSideRendering/index.js
+++ serverSideRendering/index.js
@@ -1,9 +1,25 @@
 const Vue = require('vue');
 const server = require('express')();
+const template = require('fs').readFileSync('./index.template.html', 'utf-8');
 const createApp = require("./Home/main.js")
 
-server.get('*', (req, res) => {
+const renderer = require('vue-server-renderer').createRenderer({
+    template: require('fs').readFileSync('./index.template.html', 'utf-8')
+  })
 
+
+server.get('*', (req, res) => {
+  const app = createApp()
+  renderer
+  .renderToString(app, (err, html) => {
+    console.log(app)
+    console.log(html);
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      return;
+    }
+    res.end(html);
+  });
 })
 
 server.listen(8080);
