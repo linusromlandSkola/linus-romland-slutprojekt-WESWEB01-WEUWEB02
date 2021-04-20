b48ff659b22233218574e3508574f12d2a4736aa
Linus Romland <romlandlinus@gmail.com>
2021-04-20 16:09:07 +0200

    Update loginRoutes.js

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -2,10 +2,7 @@ module.exports = (function () {
 	var express = require("express");
 	var router = express.Router();
 	
-    //Startpage
-	router.get("/", (req, res) => {
-		res.render("pages/index");
-	});
+    
 
 	return router;
 })();
