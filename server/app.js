d9d6f67a9b9210501b3ac1c6f9e875dbd1be0f36
linusromland <48645689+linusromland@users.noreply.github.com>
2021-04-20 13:55:46 +0200

    commented code

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
+//Init of dotenv for use with .env file
 require('dotenv').config();
 
+//Dependencies import
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+//Sets the view engine to EJS
 app.set("view engine", "ejs");
 
 //GET ROUTES
@@ -11,4 +14,5 @@ app.get("/", (req, res) => {
     res.render("pages/index");
 });
 
+//Starts the server
 app.listen(port, () => console.log(`FileUpload listening on port ${port}!`))
