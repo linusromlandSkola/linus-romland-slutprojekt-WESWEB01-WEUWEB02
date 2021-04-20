2055cdd3476c2d4c7673d06ee3b60ae88fbad64f
Linus Romland <romlandlinus@gmail.com>
2021-04-20 17:03:31 +0200

    some comments

--- server/app.js
+++ server/app.js
@@ -69,6 +69,7 @@ app.post(
 	})
 );
 
+//adds the loginroutes to /
 app.use("/", require("./routes/loginRoutes"));
 
 //Starts the server
--- server/loginFunction.js
+++ server/loginFunction.js
@@ -1,5 +1,6 @@
 const User = require("./models/user.js");
 
+//checks if a user is authenticated with a valid session cookie
 exports.checkAuthenticated = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		return next();
@@ -7,6 +8,7 @@ exports.checkAuthenticated = (req, res, next) => {
 	res.redirect("/login");
 };
 
+//checks if a user is authenticated with a valid session cookie and then rejects them
 exports.checkNotAuthenticated = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		return res.redirect("/auth");
@@ -14,6 +16,7 @@ exports.checkNotAuthenticated = (req, res, next) => {
 	next();
 };
 
+//function to create a usermodel from information
 exports.createUser = (nameIN, passIN) => {
 	return new User({
 	  name: nameIN,
