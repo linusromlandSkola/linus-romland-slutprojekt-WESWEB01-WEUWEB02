7c381090c2a7fc42ac3c9b0967dd36e1a2308810
Linus Romland <romlandlinus@gmail.com>
2021-04-22 13:33:23 +0200

    fixed upload and login design

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -1,4 +1,8 @@
 module.exports = (function () {
+	//Dependencies import
+	const express = require("express");
+	const router = express.Router();
+	const login = require("../login.js");
 
 	router.get("/upload", login.checkAuthenticated, (req, res) => {
 		res.render("pages/upload");
