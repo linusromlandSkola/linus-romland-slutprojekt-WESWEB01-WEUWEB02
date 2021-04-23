89e878378a0c3e078ecbad9e97b1b2f72490509b
Linus Romland <romlandlinus@gmail.com>
2021-04-23 14:16:04 +0200

    somewhat design

--- server/shared/js/upload.js
+++ server/shared/js/upload.js
@@ -1,19 +1,11 @@
-//EventListner that prevents default on form and runs function "uploadFile()"
-document
-	.getElementById("uploadForm")
-	.addEventListener("submit", function (event) {
-		event.preventDefault();
-		uploadFile();
-	});
-
 //Functions that send POST to server with file and calulates % and speed of upload
 function uploadFile() {
 	//The file to be uploaded
-    const file = document.getElementById("fileField").files[0];
+	const file = document.getElementById("fileField").files[0];
 
-    //checks if file is to large to upload
+	//checks if file is to large to upload
 	if (file.size > document.getElementById("maxFileSize").attributes[1].value) {
-        console.log("you dum dum")
+		console.log("you dum dum");
 	} else {
 		let formData = new FormData();
 		let xhr = new XMLHttpRequest();
@@ -21,17 +13,17 @@ function uploadFile() {
 		let t0 = 0;
 		let d0 = 0;
 
-        //adds to file to formData
+		//adds to file to formData
 		formData.append("file", file);
 
-        //runs when return from server
+		//runs when return from server
 		xhr.onreadystatechange = function () {
 			if (this.status == 200) {
 				console.log("cool done i thknk");
 			}
 		};
 
-        //runs during upload and calulates % & speed
+		//runs during upload and calulates % & speed
 		xhr.upload.onprogress = function (e) {
 			MBps =
 				e.loaded > 0
@@ -52,8 +44,51 @@ function uploadFile() {
 			d0 = e.loaded;
 		};
 
-        //opens and send post request to server
+		//opens and send post request to server
 		xhr.open("POST", "/uploadFile");
 		xhr.send(formData);
 	}
+}
+
+let theFile;
+
+const dropzone = document.getElementById("drop");
+const fileField = document.getElementById("fileField");
+dropzone.ondragover = () => {
+	dropzone.className = "dragover";
+	return false;
+};
+
+dropzone.ondragleave = () => {
+	dropzone.className = "";
+	return false;
+};
+
+dropzone.onclick = () => {
+	document.getElementById("fileField").click();
+};
+
+changeFile.onclick = () => {
+	document.getElementById("fileField").click();
+};
+
+dropzone.ondrop = function (e) {
+	e.preventDefault();
+	this.className = "";
+	theFile = e.dataTransfer.files[0];
+	changeContent();
+};
+
+fileField.onchange = () => {
+	console.log(fileField.files[0]);
+	if (fileField.files[0]) {
+		theFile = fileField.files[0];
+		changeContent();
+	}
+};
+
+function changeContent() {
+	document.getElementById("fileName").innerText = theFile.name;
+	document.getElementById("fakeForm").hidden = false;
+	document.getElementById("drop").style = "display:none;";
 }
